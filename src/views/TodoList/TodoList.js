f96e8aaadefb6655a33c4920b0c9684a79c86045
Hajime Takeuchi <shadowthe8156@gmail.com>
2021-01-03 17:35:19 +0900

    Todoリストの追加機能まで作成

---
+++ src/views/TodoList/TodoList.js
@@ -0,0 +1,75 @@
+import { Fragment, useState } from 'react';
+import './TodoList.css';
+import TodoItem from '../../components/TodoItem'
+
+const sampleTodoList = [
+  {
+    id: 0,
+    title: 'これはサンプル１です',
+    description: 'これはサンプル１です',
+  },
+  {
+    id: 1,
+    title: 'これはサンプル２です',
+    description: 'これはサンプル２です',
+  }
+];
+
+const TodoList = () => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [todoList, setTodoList] = useState(sampleTodoList);
+
+  const changedTitle = (e) => {
+    setTitle(e.target.value);
+  }
+
+  const changedDescription = (e) => {
+    setDescription(e.target.value);
+  }
+
+  const clickedButton = () => {
+    const newId = Math.max(...todoList.map((todo)=>todo.id)) + 1;
+    const newTodoList = todoList.slice();
+    const newTodo = {
+      id: newId,
+      title: title,
+      description: description,
+    };
+    newTodoList.push(newTodo);
+    setTodoList(newTodoList);
+    setTitle('');
+    setDescription('');
+  }
+
+  return (
+    <Fragment>
+      <div>
+        <input 
+          className="todo-title-input" 
+          type="text" 
+          value={title} 
+          onChange={changedTitle} 
+        />
+        <textarea 
+          className="todo-description-input" 
+          value={description} 
+          onChange={changedDescription} 
+        />
+      </div>
+      <div>
+        <button 
+          className="todo-add-button" 
+          onClick={clickedButton}
+        >
+          Click Me!!!
+        </button>
+      </div>
+      {todoList.map((todo) => {
+        return <TodoItem todo={todo} key={todo.id} />;
+      })}
+    </Fragment>
+  )
+}
+
+export default TodoList;
